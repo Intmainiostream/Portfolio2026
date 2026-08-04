70cf3f2d41d2ee4c4b0774cc64522f73fb76de25
Intmainiostream <joshuaxd050@gmail.com>
2026-08-04 12:56:49 +0800

    done.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,7 @@
         })();
     </script>
     <title>{{ $title ?? 'Joshua Dela Cruz' }}</title>
+    <link rel="icon" href="{{ asset('images/joshua-light.png') }}" type="image/png">
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
     <link href="https://fonts.googleapis.com/css2?family=Instrument+Sans:wght@400;500;600&display=swap" rel="stylesheet" />
